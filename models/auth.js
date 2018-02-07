06832b6dd30b1fb9df81503670898e036c58d879
Finalgalaxy <finalgalaxy@gmail.com>
2018-02-07 23:38:12 +0100

    Added authentication module.

--- models/auth.js
+++ models/auth.js
@@ -2,8 +2,12 @@ var bCrypt = require("bcrypt");
 var bCrypt_salt=10;
 
 function Auth(){
-    this.encryptPassword = function(password){
-        return bCrypt.hashSync(password, 10);
+    this.encryptPassword = function(password, callback){
+        bCrypt.hash(password, 10, callback);
+    }
+
+    this.checkPassword = function(password, saved_password, callback){
+        bCrypt.compare(password, saved_password, callback);
     }
 }
 
